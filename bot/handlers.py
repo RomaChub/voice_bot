47db8363945d90138ae2ff0819398e41bbe0ec8d
Roma <5658031.rch@gmail.com>
2024-06-12 15:02:27 +0300

    1.0

--- bot/handlers.py
+++ bot/handlers.py
@@ -2,7 +2,7 @@ from aiogram.types import Message, Voice, File
 from aiogram import Router, F, Bot, types
 from aiogram.filters import CommandStart
 
-from bot.utils import save_voice_as_mp3
+from bot.utils import save_voice_as_mp3, audio_to_text
 
 router = Router()
 
@@ -15,5 +15,6 @@ async def start(message: Message):
 @router.message(F.content_type == "voice")
 async def process_voice_message(message: Message, bot: Bot):
     voice_path = await save_voice_as_mp3(bot, message)
+    #transcripted_voice_text = await audio_to_text(voice_path)
 
-    await message.reply(text="all is good!")
+    await message.reply(text=voice_path)
--- bot/utils.py
+++ bot/utils.py
@@ -1,5 +1,11 @@
+import openai
 from aiogram import Bot
 from aiogram.types import Message
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
 async def save_voice_as_mp3(bot: Bot, message: Message):
@@ -9,4 +15,12 @@ async def save_voice_as_mp3(bot: Bot, message: Message):
     file_name = f"files/audio{file_id}.mp3"
     voice_mp3_path = await bot.download_file(file_path, file_name)
 
-    return voice_mp3_path
+    return file_name
+
+
+async def audio_to_text(file_path: str) -> str:
+    with open(file_path, "rb") as audio_file:
+        transcript = await openai.Audio.atranscribe(
+            "whisper-1", audio_file
+        )
+    return transcript["text"]
