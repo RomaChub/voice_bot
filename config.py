f02889ece47393dc84313d74fba611bb7e745430
Roma <5658031.rch@gmail.com>
2024-06-18 00:39:42 +0300

    add my_values funk

--- config.py
+++ config.py
@@ -8,19 +8,20 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    telegram_token: str = Field(os.getenv("TELEGRAM_TOKEN"))
-    openai_api_key: str = Field(os.getenv("OPENAI_API_KEY"))
 
-    db_host: str = Field(os.getenv("DB_HOST"))
-    db_port: int = Field(os.getenv("DB_PORT"))
-    db_name: str = Field(os.getenv("DB_NAME"))
-    db_user: str = Field(os.getenv("DB_USER"))
-    db_pass: str = Field(os.getenv("DB_PASS"))
+    telegram_token: str = os.getenv("TELEGRAM_TOKEN")
+    openai_api_key: str = os.getenv("OPENAI_API_KEY")
 
-    assistant_id: str = Field(os.getenv("ASSISTANT_ID"))
-    value_assistant_id: str = Field(os.getenv("VALUE_ASSISTANT_ID"))
+    db_host: str = os.getenv("DB_HOST")
+    db_port: int = os.getenv("DB_PORT")
+    db_name: str = os.getenv("DB_NAME")
+    db_user: str = os.getenv("DB_USER")
+    db_pass: str = os.getenv("DB_PASS")
 
-    postgres_volume_path: str = Field(os.getenv("POSTGRES_VOLUME_PATH"))
+    assistant_id: str = os.getenv("ASSISTANT_ID")
+    value_assistant_id: str = os.getenv("VALUE_ASSISTANT_ID")
+
+    postgres_volume_path: str = os.getenv("POSTGRES_VOLUME_PATH")
 
     @property
     def get_database_url(self):
